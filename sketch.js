6ed70fe5f05e0a0abcee5e5234cc66a948c3e4c6
uvegesmate <73605340+uvegesmate@users.noreply.github.com>
2021-04-13 20:50:41 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,18 +1,33 @@
 var img;
-let cnv,
+
+var radiation;
+
+var font;
+
 
 function preload() {
   img = loadImage('data/panel2.jpg');
+  radiation = loadSound("data/radiation.aac");
+  font = loadFont("data/digital-7.ttf");
 }
 
 function setup() {
-  cnv = createCanvas(700, 1000);
-  let newCanvasX = (windowWidth - 700)/2;
-  let newCanvasY = (windowHeight - 1000)/2;
-  cnv.position(newCanvasX, newCanvasY);
+  
+  radiation.loop();
+  
+  
+  
 }
 
 function draw() {
+  createCanvas(windowWidth, windowHeight);
+  
+  
+  
+  radiation.rate(1+mouseX*0.001+mouseY*0.001);
+  
+  
+  
   var tileCountX = mouseX / 10 + 1;
   var tileCountY = mouseY / 10 + 1;
   var stepX = width / tileCountX;
@@ -22,6 +37,12 @@ function draw() {
       image(img, gridX, gridY, stepX, stepY);
     }
   }
+  textFont(font);
+  fill(255,255,255,0+mouseX*0.2+mouseY*0.1,);
+  textSize(50);
+  translate(windowWidth/2, windowHeight/2);
+  textAlign(CENTER, CENTER)
+  text("CHERNOBYL", 0,0);
 }
 
 function keyReleased() {
